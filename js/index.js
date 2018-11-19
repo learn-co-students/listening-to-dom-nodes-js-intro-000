95f06bb089fd4a6a1bac224e3737259627c120eb
Fei Qi <feiqi9047@gmail.com>
2018-11-19 23:14:19 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const divs = document.querySelectorAll('div');
+// you can also load the code snippets in using your browser's console	
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+     console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
