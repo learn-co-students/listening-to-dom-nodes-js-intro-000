541c9e9de592df778cb9871042a5470376d2d66e
Andrei Korchagin <andrei.korchagin@gmail.com>
2019-02-07 15:09:07 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+divs = document.querySelectorAll('div');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
