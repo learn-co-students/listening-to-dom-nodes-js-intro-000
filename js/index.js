4122bcfb6d4af59958788b54f8852490a1181813
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-19 05:15:50 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+main.addEventListener('click', (e) => alert('I was clicked'));
