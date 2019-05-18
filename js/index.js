1e8263a9ccea55094966005e0468b31d09272211
Doug Loomer <loomerds@gmail.com>
2019-05-18 14:53:31 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,46 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+window.onload=function(){
+  /*var main = document.getElementById('main');
+  main.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
+
+  const input = document.querySelector('input');
+
+  input.addEventListener('keydown', function(e) {
+    if (e.key === "g") {
+    return e.preventDefault()
+    } else {
+      console.log(e.key)
+    }
+  });
+
+  let divs = document.querySelectorAll('div');
+
+  function bubble(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+  }
+
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+  }
+
+  function capture(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' captured');
+  }
+
+  for (let i = 0; i < divs.length; i++) {
+    // set the third argument to `true`!
+    divs[i].addEventListener('click', capture, true);
+  }*/
+
+  const divs = document.querySelectorAll('div');
+
+  function bubble(e) {
+    e.stopPropagation();
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+  }
+
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+  }
+}
