b00d4e2ff1d8f1dc768ed9e1121631dedead1557
E.B <edenbenbow@yahoo.com>
2019-08-14 21:18:11 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  sonsole.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
