c09235972dfc5721a3d7d6f60b975761136930bf
Ravi <kkiran.ror@gmail.com>
2020-03-15 03:47:49 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,61 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
+//Demonstrate Triggering Events Listeners on DOM Nodes
+const input = document.querySelector('input')
+ //Demonstrate Preventing the Default Behavior for DOM Nodes
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+});
+
+//bubbling
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
+//capturing
+divs = document.querySelectorAll('div');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
+
+//Demonstrate Stopping Propagated Behaviors with stopPropagation()
+const divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
