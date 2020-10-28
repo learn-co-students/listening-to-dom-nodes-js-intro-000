9f05c3d64f3dfb80afa1eb82a2b18ea6ce9c790d
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-28 14:21:19 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,14 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-const input = document.querySelector('input')
-
-input.addEventListener('keydown', function(e) {
-  if (e.key === "g") {
-    return e.preventDefault()
-  } else {
-    console.log(e.key)
-  }
-});
-
-let divs = document.querySelectorAll('div');
+const divs = document.querySelectorAll('div');
 
 function bubble(e) {
-  // remember all of those fancy DOM node properties?
-  // we're making use of them to get the number
-  // in each div here!
+  // stop! that! propagation!
+  e.stopPropagation();
 
-  // if `this` is a bit confusing, don't worry —
-  // for now, know that it refers to the div that
-  // is triggering the current event handler.
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
 
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
-}
-
-divs = document.querySelectorAll('div');
-
-function capture(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' captured');
-}
-
-for (let i = 0; i < divs.length; i++) {
-  // set the third argument to `true`!
-  divs[i].addEventListener('click', capture, true);
 }
