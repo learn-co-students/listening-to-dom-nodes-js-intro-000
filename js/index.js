16c500876a864f8dc4f34ca702011c190e582a0c
lisswart <arnoudlisa@gmail.com>
2021-03-21 22:43:41 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for(let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+}
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
